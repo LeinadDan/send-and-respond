b818e195d3ca4a6b9b0db60055fb50427e961d22
Daniel Teodoro <i.am.daniel08@gmail.com>
2018-07-07 22:24:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
-<?php
-    echo json_encode($_POST);
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Send and Respond</title>
+</head>
+
+<body>
+    <h1>Lorem Ipsum</h1>
+    <div id="lipsum">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus neque arcu, tristique pellentesque mauris ut, gravida feugiat metus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pulvinar laoreet vestibulum. Vestibulum blandit, nisi at eleifend convallis, tortor urna mattis erat, quis fringilla nulla augue id risus. Nulla sit amet mi magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer ullamcorper ac tortor a pellentesque. Suspendisse sed condimentum nisl, sit amet laoreet tortor. Donec aliquam porttitor ex, non sollicitudin dui ornare a. Praesent rutrum laoreet cursus. Nullam eget tincidunt dolor. Nam cursus scelerisque efficitur. Nam rutrum dolor vitae cursus vehicula.
+        </p>
+        <p>
+            Nunc rutrum diam odio. Vivamus a velit vulputate, cursus velit nec, mollis risus. Nam lacinia turpis ac suscipit ultrices. Nam vel sollicitudin nisl. Phasellus interdum nec lorem nec efficitur. Cras luctus nulla in commodo tincidunt. Sed fermentum luctus augue id egestas.
+        </p>
+        <p>
+            Nullam volutpat diam non diam consequat dictum. Phasellus mattis tempus libero, ut ornare lacus commodo vitae. Morbi et tincidunt nibh. Etiam vitae nibh id ipsum aliquet ullamcorper. Quisque non volutpat nisi. Aliquam erat volutpat. Aliquam ut diam ut risus tristique commodo. Aliquam sit amet enim nec augue tempor cursus ut non libero. Praesent sed ante maximus, vulputate tellus eget, iaculis lorem. Fusce in velit nec diam mollis faucibus ac et lorem. Pellentesque odio neque, varius quis sem sit amet, tempor ultricies est.
+        </p>
+        <p>
+            Proin sit amet massa urna. Sed ipsum leo, consequat non porttitor vel, auctor quis tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam diam sem, euismod tempor neque a, fringilla hendrerit ante. Integer consequat nisi elit, id efficitur mi lobortis ac. Aenean aliquet auctor purus, rhoncus malesuada sapien ultricies sed. Curabitur convallis nisl vitae magna tristique fringilla. Donec rutrum augue magna, sed consectetur augue sollicitudin sit amet. Fusce venenatis tempus egestas. Phasellus imperdiet quam quam, sit amet porta tortor cursus quis. Cras sed lacus sit amet mi tempus mollis eget eget urna.
+        </p>
+        <p>
+            Ut commodo justo ac nisl convallis tincidunt. Nunc nec efficitur mauris. Nullam malesuada tellus pretium, mattis diam ornare, auctor sem. Etiam sed tempor tellus, non congue ex. Mauris eu elit sed erat euismod ultrices. Integer luctus laoreet aliquam. Donec tempor, leo vel tempus rutrum, mi elit scelerisque libero, vulputate sodales ipsum mauris ut sem. Sed eget porta nibh. Donec eget rhoncus lectus. Aliquam non aliquam dolor, ut scelerisque nisl.
+        </p>
+    </div>
+
+</body>
+</html>
